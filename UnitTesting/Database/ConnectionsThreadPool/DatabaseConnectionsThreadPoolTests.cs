3783a98bc9064d10efcada31fd35742ec5eb3a0f
aayas <aayash@mail.com>
2024-11-03 01:19:30 +0200

    Database connection and validation unit tests

---
+++ UnitTesting/Database/ConnectionsThreadPool/DatabaseConnectionsThreadPoolTests.cs
@@ -0,0 +1,73 @@
+﻿namespace UnitTesting.Database.ConnectionsThreadPool
+{
+    using DatabaseCoreKit.DatabaseConnectionsThreadPool;
+    using Microsoft.Data.SqlClient;
+
+    [TestClass]
+    public class DatabaseConnectionsThreadPoolTests
+    {
+        private DatabaseConnectionsThreadPool _databaseConnectionsThreadPool;
+        private const int maxDatabaseConnections = 10;
+        
+        public DatabaseConnectionsThreadPoolTests()
+        {
+            this._databaseConnectionsThreadPool = DatabaseConnectionsThreadPool.GetDatabaseConnectionInstance();
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionsThreadPoolInstance()
+        {
+            Assert.IsNotNull(this._databaseConnectionsThreadPool);
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionNotvalidTest()
+        {
+            Assert.IsNotNull(this._databaseConnectionsThreadPool.GetDatabaseConnection());
+        }
+
+        [TestMethod]
+        public void TestAvailableDatabaseConnections()
+        {
+            this._databaseConnectionsThreadPool = DatabaseConnectionsThreadPool.GetDatabaseConnectionInstance();
+
+            for(int index = 0; index < maxDatabaseConnections; index++)
+            {
+                var connection = _databaseConnectionsThreadPool.GetDatabaseConnection();
+            }
+
+            Assert.AreEqual(_databaseConnectionsThreadPool.AvailableConnectionsCount, 0);
+        }
+
+        [TestMethod]
+        public void TestDatbaseConnectionPoolCapacityGrow()
+        {
+            const int maxConnections = 10; // Max connections by default
+            this._databaseConnectionsThreadPool = DatabaseConnectionsThreadPool.GetDatabaseConnectionInstance();
+
+            for (int index = 0; index < maxConnections; index++)
+            {
+                var connection = _databaseConnectionsThreadPool.GetDatabaseConnection();
+            }
+
+            var maxCapacityConnection = _databaseConnectionsThreadPool.GetDatabaseConnection();
+
+            Assert.IsTrue(_databaseConnectionsThreadPool.CurrentlyUsedConnections > maxConnections);
+        }
+
+
+        [TestMethod]
+        public void TestDatbaseConnectionRelease()
+        {
+            this._databaseConnectionsThreadPool = DatabaseConnectionsThreadPool.GetDatabaseConnectionInstance();
+
+            var connection = _databaseConnectionsThreadPool.GetDatabaseConnection();
+            if (connection == null)
+                return;
+
+            bool isReleased = _databaseConnectionsThreadPool.ReleaseUsedConnection(connection);
+
+            Assert.IsTrue(isReleased);
+        }
+    }
+}
---
+++ UnitTesting/Database/DatabaseConnectionTester/DatabaseConnectionValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿namespace UnitTesting.Database.DatabaseConnectionTest
+{
+    using DatabaseCoreKit.DatabaseConnectionsThreadPool;
+    using DatabaseCoreKit.DatabaseConnectionValidator;
+
+    [TestClass]
+    public class DatabaseConnectionValidatorTests
+    {
+        private readonly DatabaseConnectionValidator _databaseConnectionValidator;
+        private readonly DatabaseConnectionsThreadPool _databaseConnectionsThreadPool;
+        public DatabaseConnectionValidatorTests()
+        {
+            this._databaseConnectionValidator = new DatabaseConnectionValidator();
+            this._databaseConnectionsThreadPool = DatabaseConnectionsThreadPool.GetDatabaseConnectionInstance();
+        }
+
+        [TestMethod]
+        public void TestIfClosedConnectionIsValid()
+        {
+            var databaseConnection = _databaseConnectionsThreadPool.GetDatabaseConnection();
+            if (databaseConnection == null)
+                return;
+
+            databaseConnection.Close();
+
+            bool isConnectionOpened =  _databaseConnectionValidator.IsConnectionOpen(databaseConnection);
+            Assert.IsFalse(isConnectionOpened);
+        }
+    }
+}
