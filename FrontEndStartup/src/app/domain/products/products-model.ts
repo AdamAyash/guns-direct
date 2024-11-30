a1f05aaaa0567edb22f15f4881fd1182dea0fe44
pavel <pavel@DESKTOP-CCIMPQA>
2024-11-30 19:15:54 +0200

    Product Model design

--- FrontEndStartup/src/app/domain/products/products-model.ts
+++ FrontEndStartup/src/app/domain/products/products-model.ts
@@ -1,3 +1,24 @@
 export class Product{
-    
+    id: number;
+    name: string;
+    price: number;
+    category: string;
+    quantity: number;
+    imageURL: string;
+
+    constructor(
+        id: number,
+        name: string,
+        price: number,
+        category: string,
+        quantity: number,
+        imageURL: string 
+    ) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.quantity = quantity;
+        this.imageURL = imageURL;
+    }
 }
